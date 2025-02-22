f56731691030a14130e06ce2d0b767613137df05
ZakirAliyev <aliyevzakir814@gmail.com>
2025-02-22 23:56:49 +0400

    Boiler Plate React

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar/index.jsx";
+import Footer from "../../components/Footer/index.jsx";
+
+function HomePage() {
+    return (
+        <section id={"homePage"}>
+            <Navbar/>
+            <Footer/>
+        </section>
+    );
+}
+
+export default HomePage;
